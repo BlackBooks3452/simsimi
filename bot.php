d8d5a31bc16a35b35aa012453d381c56b597073b
BlackBooks3452 <33177396+BlackBooks3452@users.noreply.github.com>
2017-11-01 19:15:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
+$key = '453e8070-ab12-491a-ad66-f9fc9038b2d9'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
